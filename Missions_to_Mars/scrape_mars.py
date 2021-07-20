1e9ee865c2554f5670e9c61706f31e7f101dee02
jmgeo <j.mills.geo@gmail.com>
2021-07-19 23:39:05 -0500

    Working on step 2. Created scrape_mars.py file and imported the dependencies. Working on the code.

---
+++ Missions_to_Mars/scrape_mars.py
@@ -0,0 +1,8 @@
+#import dependencies
+import pandas as pd
+from splinter import Browser
+from selenium import webdriver
+from bs4 import BeautifulSoup as bs
+from webdriver_manager.chrome import ChromeDriverManager
+
+
