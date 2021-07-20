29266ce72b3473a74b036417bed67135547979e5
jmgeo <j.mills.geo@gmail.com>
2021-07-20 02:09:08 -0500

    Defined the scraping function.

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -5,4 +5,16 @@ from selenium import webdriver
 from bs4 import BeautifulSoup as bs
 from webdriver_manager.chrome import ChromeDriverManager
 
+#define the scraping function
+def scrape():
+    # FEATURED IMAGE SCRAPE
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    url = "https://spaceimages-mars.com/"
+    browser.visit(url)
+    html = browser.html
+    soup = bs(html, "html.parser")
+    img = [i.get("src") for i in soup.find_all("img", class_="headerimage fade-in")]
+    featured_image_url = url + img[0]
 
